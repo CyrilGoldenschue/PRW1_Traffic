f3f03bdb5853ebd9217096b8509de6dc5dfe3d24
CyrilGoldenschue <cyril.goldenschue@cpnv.ch>
2021-08-31 15:50:10 +0200

    Add Session

--- index.php
+++ index.php
@@ -1,9 +1,17 @@
 <?php
+session_start();
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 require_once "Controller/Controller.php";
 
+$_SESSION['state'] = isset($_POST['state']) ? $_POST['state'] : 0;
+$_SESSION['action'] = isset($_POST['action']) ? $_POST['action'] : 0;
 
-if(isset($_POST["action"])) {
-    switch ($_POST["action"]) {
+if(isset($_SESSION["action"])) {
+    switch ($_SESSION["action"]) {
         case "Next":
             NextState();
             break;
@@ -13,11 +21,11 @@ if(isset($_POST["action"])) {
             break;
 
         default:
-            IncludeView($_POST['state']);
+            IncludeView();
             break;
     }
 }else{
-    IncludeView($_POST['state']);
+    IncludeView();
 }
 
 ?>
