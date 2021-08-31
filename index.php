9f2f22ee1bea6c2165246ca9a12124b8fcf87d57
CyrilGoldenschue <cyril.goldenschue@cpnv.ch>
2021-08-31 14:16:26 +0200

    Use button to change state

--- index.php
+++ index.php
@@ -1,1 +1,23 @@
 <?php
+require_once "Controller/Controller.php";
+
+
+if(isset($_POST["action"])) {
+    switch ($_POST["action"]) {
+        case "Next":
+            NextState();
+            break;
+
+        case "HS":
+            HS();
+            break;
+
+        default:
+            IncludeView($_POST['state']);
+            break;
+    }
+}else{
+    IncludeView($_POST['state']);
+}
+
+?>
